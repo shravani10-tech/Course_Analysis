29827d6db53e64140c36fe4d68cf1bda2f928f22
Shravani Kadam <84696409+shravani10-tech@users.noreply.github.com>
2023-05-04 12:25:14 +0530

    Add files via upload

---
+++ Course-Sentiment-Analysis-main/Course-Sentiment-Analysis-main/app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request
+from webScraping import WebScraping
+from mlmodel import Ratings
+import pickle
+
+app = Flask(__name__)
+
+CourseInfo = {
+    'totalCourses': 0,
+    'courses': []
+}
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/analysis', methods=['GET', 'POST'])
+def analysis():
+    if request.method == 'GET':
+        return render_template('analysisForm.html')
+
+    if request.method == 'POST':
+        link = request.form.get('link')
+        site = request.form.get('platform')
+        CourseInfo['totalCourses'] = 1
+        CourseInfo['courses'].clear()
+        CourseInfo['courses'].append(WebScraping(link, site))
+        # model = pickle.load(open('model.pkl','rb'))
+        # CourseInfo['courses'][0]['rating'] = model(CourseInfo.comments)
+        CourseInfo['courses'][0]['rating'] = Ratings(CourseInfo['courses'][0]['comments'])
+        CourseInfo['courses'][0]['platform'] = site
+        CourseInfo['courses'][0]['link'] = link
+        return render_template('analysisReport.html', CourseInfo=CourseInfo)
+
+
+@app.route('/multipleAnalysis', methods=['GET', 'POST'])
+def multipleAnalysis():
+    if request.method == 'POST':
+        if request.form.get('platform'):
+            link = request.form.get('link')
+            site = request.form.get('platform')
+            CourseInfo['totalCourses'] += 1
+            CourseInfo['courses'].append(WebScraping(link, site))
+            # WebsScraping will return dictoionary as:
+            # CourseInfo = {
+            #     'totalCourses': 0,
+            #     'courses': [
+            #         {
+            #             'title': 'xyz',
+            #             'duration': '34 hr',
+            #             'instructor': 'Maruti Patil',...,
+            #             'comments':[
+            #                 'Hello WOrld',
+            #                 'Great course',...
+            #             ]
+            #         },...
+            #     ]
+            # }
+
+            # We will load the model here and after calling it with list of comments as parameter it should return rating
+            # CourseInfo['courses'][-1]['rating'] = model(CourseInfo.comments)
+            CourseInfo['courses'][-1]['rating'] = Ratings(CourseInfo['courses'][-1]['comments'])
+            CourseInfo['courses'][-1]['platform'] = site
+            CourseInfo['courses'][-1]['link'] = link
+        return render_template('multipleAnalysisReport.html', CourseInfo=CourseInfo)
+
+
+# TODO
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutUs.html')
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
---
+++ Course-Sentiment-Analysis-main/Course-Sentiment-Analysis-main/webScraping.py
@@ -0,0 +1,139 @@
+from bs4 import BeautifulSoup
+import urllib.request, json
+import requests
+
+# def WebScraping(link,site):
+#     return {
+#         'title' : 'Flask Course',
+#         'instructor' : 'Mandar Patil',
+#         'duration' : '12 hr',
+#         'learner_count': '12421 total leaners',
+#         'description': 'Start Learning Flask from complete start. We cover all the main topics in best way possible compared to all the available plaform till the date',
+#         'comments' : ['Hello World','Great course']
+#     }
+
+def WebScraping(link, platform):
+    site = platform
+    c_url = link
+    comments = []
+    c = []
+
+    result = {}
+    if site == "Coursera":
+        result['platform'] = 'Coursera'
+        if '?' in c_url:
+            c_url = c_url[:c_url.index("?")]
+        url = c_url
+        response = requests.get(url)
+        htmlcontent = response.content
+        soup = BeautifulSoup(htmlcontent, "html.parser")
+        c = soup.findAll('h1', {'class': 'banner-title banner-title-without--subtitle m-b-0'})
+        for i in c:
+            result["title"] = i.text
+        c = soup.findAll('div', {'class': 'content-inner'})
+        for i in c:
+            result["description"] = i.text
+        c = soup.findAll('div', {'class': '_16ni8zai m-b-0 m-t-1s'})
+        for i in c:
+            result["duration"] = i.text
+        c = soup.findAll('h3', {'class': 'instructor-name headline-3-text bold'})
+        for i in c:
+            result["instructor"] = i.text
+        c = soup.findAll('div', {'class': '_1fpiay2'})
+        for i in c:
+            result["learner_count"] = i.text
+
+        res=[]
+
+        url1 = url + "/reviews"
+        response = requests.get(url1)
+        htmlcontent = response.content
+        soup = BeautifulSoup(htmlcontent, "html.parser")
+        container = soup.findAll('div', {'class': 'css-18w79dz'})
+        # res.append(container.text)
+            # for j in container:
+            #     comments.append(j.text)
+        # for j in container:
+        #     comments.append(j.text)
+        for item in container:
+            res.append(item.text)
+
+        result["comments"] =res
+        print(result)
+
+        return result
+
+    elif site =="Youtube":
+        result['v'] = link[link.index('=')+1:]
+        videoId = c_url[(c_url.index("=") + 1):]
+        url = 'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&id={}&key={}'.format(
+            videoId, 'AIzaSyDEgmEzXQ7GCRXqAa8ctgc6jA50vZJLhR4')
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        data = json.loads(data)
+        duration = data["items"][0]["contentDetails"]["duration"]
+        result["duration"] = ''
+        if duration.find('H')!=-1:
+            result["duration"] = result["duration"] + (duration[duration.find('T') + 1:duration.find('H')] + ' Hr ')
+        if duration.find('M')!=-1 and duration.find('H')!=-1:
+            result["duration"] = result["duration"] + ' ' + (duration[duration.find('H') + 1:duration.find('M')] + ' Mins')
+        elif duration.find('M')!=-1:
+            result["duration"] = result["duration"] + ' ' +(duration[duration.find('T') + 1:duration.find('M')] + ' Mins')
+
+        url = 'https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id={}&key={}'.format(
+            videoId, 'AIzaSyDEgmEzXQ7GCRXqAa8ctgc6jA50vZJLhR4')
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        data = json.loads(data)
+        result["learner_count"] = data["items"][0]["statistics"]["viewCount"] + ' views'
+
+        url = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id={}&key={}'.format(
+            videoId, 'AIzaSyDEgmEzXQ7GCRXqAa8ctgc6jA50vZJLhR4')
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        data = json.loads(data)
+        result["platform"] = "YouTube"
+        result["title"] = data["items"][0]["snippet"]["title"]
+        result["instructor"] = data["items"][0]["snippet"]["channelTitle"]
+        result["description"] = data["items"][0]["snippet"]["description"]
+
+        url = 'https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=100&videoId={}&key={}'.format(
+            videoId, 'AIzaSyDEgmEzXQ7GCRXqAa8ctgc6jA50vZJLhR4')
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        data = json.loads(data)
+        for i in range(len(data["items"])):
+            if data['items'][i]['snippet']['topLevelComment']['snippet']['textDisplay'] is not None:
+                comments.append(data['items'][i]['snippet']['topLevelComment']['snippet']['textDisplay'])
+        result["comments"] = comments
+        return result
+    
+    elif site == "Udemy":
+        videoId = c_url[51:(c_url.index('?')) - 1]
+        description_link="https://www.udemy.com/course/{}".format(videoId)
+        url = 'https://www.udemy.com/api-2.0/courses/{}/'.format(videoId)
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        data = json.loads(data)
+        result["platform"] = "Udemy"
+        result["title"] = data["title"]
+        result["instructor"] = data["visible_instructors"][0]["display_name"]
+        response = requests.get(url)
+        htmlcontent = response.content
+        # soup = BeautifulSoup(htmlcontent, "html.parser")
+        # c = soup.findAll('div', {'data-purpose': 'safely-set-inner-html:description:description'})
+        result["description"] = 'N.A'
+        result["duration"] = 'N.A'
+        result["learner_count"] = 'N.A'
+
+
+        for i in range(1, 10):
+            url = 'https://www.udemy.com/api-2.0/courses/{}/reviews/?page={}'.format(videoId, i)
+            response = urllib.request.urlopen(url)
+            data = response.read()
+            data = json.loads(data)
+            for j in range(len(data["results"])):
+                if data["results"][j]["content"]!='':
+                    comments.append(data["results"][j]["content"])
+        result["comments"] = comments
+        return(result)
